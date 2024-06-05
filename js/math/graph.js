d4512f6f81558ddfbc1aa24f010526dbc47fe3f0
ander <andercat053@yahoo.com>
2024-06-05 19:38:10 +0800

    graph draw basic function

--- js/math/graph.js
+++ js/math/graph.js
@@ -16,8 +16,30 @@ class Graph{
 		return this.segments.find((s) => s.equals(segment));
 	}
 
+	removePoint(point){
+		const segs = this.getSegementWithPoints(point);
+		for(const seg of segs){
+			this.removeSegment(seg);
+		}
+		this.points.splice(this.points.indexOf(point), 1);
+	}
 	removeSegment(segment){
 		this.segments.splice(this.segments.indexOf(segment), 1);
+	}
+
+	getSegementWithPoints(point){
+		const segs = [];
+		for(const seg of this.segments){
+			if(seg.includes(point)){
+				segs.push(seg);
+			}
+		}
+		return segs;
+	}
+
+	dispose(){
+		this.points.length = 0;
+		this.segments.length = 0;
 	}
 
 	tryAddSegment(segment){
